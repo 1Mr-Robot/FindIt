1e9532b47bb17bd9225401dd454d82d995a3c4c5
Uziel Flores <uzielo8017518@gmail.com>
2026-04-14 23:43:47 -0600

    Creacion de logica para el singin

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model
 
+#Login
 class LoginForm(AuthenticationForm):
     username = forms.EmailField(label='Correo institucional')
 
@@ -18,3 +20,48 @@ class LoginForm(AuthenticationForm):
             )
 
         return email
+    
+#Register
+User = get_user_model()
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = [
+            'institutional_email',
+            'tuition',
+            'first_name',
+            'last_name',
+            'phone',
+            'role'
+        ]
+
+    def clean_institutional_email(self):
+        email = self.cleaned_data.get('institutional_email')
+
+        if not email.endswith('@uanl.edu.mx'):
+            raise forms.ValidationError('El correo debe ser institucional')
+
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm = cleaned_data.get("confirm_password")
+
+        if password != confirm:
+            raise forms.ValidationError("Las contraseñas no coinciden")
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])  # Encripta la contraseña
+
+        if commit:
+            user.save()
+
+        return user
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from .views import register_view
 from .forms import LoginForm
 
 urlpatterns = [
@@ -8,5 +9,6 @@ urlpatterns = [
         auth_views.LoginView.as_view(
             template_name='users/login.html',
             authentication_form=LoginForm
-        ), name='login')
+        ), name='login'),
+    path('register/', register_view, name='register'),
 ]
