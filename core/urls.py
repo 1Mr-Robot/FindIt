6382df0e46451428908bb3a7008708efbf3c295e
Uziel Flores <uzielo8017518@gmail.com>
2026-04-23 23:41:02 -0600

    Creacion de modal de info de item

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('item_found/<int:item_id>/', views.item_found, name='item_found'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import Item
@@ -20,5 +20,14 @@ def home(request):
 
     return render(request, 'core/home.html', {
         'items': items,
-        'query': query
+        'query': query,
+        'is_staff': request.user.is_staff
     })
+
+@login_required
+def item_found(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    if request.user.is_staff:
+        item.status = Item.Status.FOUND
+        item.save()
+    return redirect('home')
