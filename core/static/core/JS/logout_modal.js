941750dc9e8bb85461694374ff621ecc29ded004
Uziel Flores <uzielo8017518@gmail.com>
2026-04-23 23:09:12 -0600

    Creacion de boton del panel de admin y funcionalidad al boton de cerrar sesion en el sidebar

---
+++ core/static/core/JS/logout_modal.js
@@ -0,0 +1,6 @@
+function openLogoutModal() {
+    document.getElementById('logout-modal').classList.remove('hidden');
+}
+function closeLogoutModal() {
+    document.getElementById('logout-modal').classList.add('hidden');
+}
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from .views import login_view, register_view
 
 urlpatterns = [
     path('', login_view.as_view(), name='login'),
     path('register/', register_view, name='register'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
